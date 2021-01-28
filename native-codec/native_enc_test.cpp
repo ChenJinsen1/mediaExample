a5e256d3b005f29431b5caf5ef2e869f1334f50e
Chen Jinsen <kevin.chen@rock-chips.com>
2021-01-28 21:21:58 +0800

    native-codec: add licence declare
    Signed-off-by: Chen Jinsen <kevin.chen@rock-chips.com>

--- native-codec/native_enc_test.cpp
+++ native-codec/native_enc_test.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2021 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * author: kevin.chen@rock-chips.com
+ *   date: 2021/01/28
+ * module: native-codec: native_enc_test sample code
+ */
+
 //#define LOG_NDEBUG 0
 #define LOG_TAG "native_enc_test"
 #include <utils/Log.h>
