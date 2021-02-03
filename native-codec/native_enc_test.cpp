14a416b19433e1be70030b91b7e8c52652426c29
Chen Jinsen <kevin.chen@rock-chips.com>
2021-02-03 20:05:12 +0800

    [native-codec] restructure enc_test
    Signed-off-by: Chen Jinsen <kevin.chen@rock-chips.com>

--- native-codec/native_enc_test.cpp
+++ native-codec/native_enc_test.cpp
@@ -37,47 +37,30 @@
 
 using namespace android;
 
+#define MAX_FILE_LEN 128
+
 static const char* kMimeTypeAvc = "video/avc";
+static int64_t kTimeout = 20 * 1000; // 20ms
 
-typedef struct {
-    struct timeval start;
-    struct timeval end;
-} DebugTimeInfo;
-
-static DebugTimeInfo time_info;
-
-static void time_start_record()
-{
-    gettimeofday(&time_info.start, NULL);
-}
-
-static uint64_t time_end_record()
-{
-    gettimeofday(&time_info.end, NULL);
-    return (time_info.end.tv_sec  - time_info.start.tv_sec)  * 1000 +
-           (time_info.end.tv_usec - time_info.start.tv_usec) / 1000;
-}
-
-typedef struct EncTestData_t {
+typedef struct EncTestArgs_t {
     // src and dst
-    FILE                *fp_input;
-    FILE                *fp_output;
-    char                file_input[128];
-    char                file_output[128];
-    uint32_t            frameCount;
+    char                 file_input[MAX_FILE_LEN];
+    char                 file_output[MAX_FILE_LEN];
+    FILE                 *fp_input;
+    FILE                 *fp_output;
 
     // configure parameters of encoder
-    Vector<sp<ABuffer>>  inBuffers;
-    Vector<sp<ABuffer>>  outBuffers;
+    sp<MediaCodec>       codec;
+    Vector<sp<ABuffer> > inBuffers;
+    Vector<sp<ABuffer> > outBuffers;
     uint32_t             width;
     uint32_t             height;
     uint32_t             bitRate;
     uint32_t             colorFormat;
     uint32_t             frameRate;
     uint32_t             IDRInterval;
-    // codec timeout value
-    int64_t              timeout;
-} EncTestData;
+    uint32_t             numBuffersEncoded;
+}  EncTestArgs;
 
 /*
  * Dumps usage on stderr.
@@ -106,7 +89,7 @@ static void testUsage() {
         "\n");
 }
 
-status_t testSetupEncDataByArgs(EncTestData *data, int argc, char **argv) {
+status_t testParseArgs(EncTestArgs *cmd, int argc, char **argv) {
     static const struct option longOptions[] = {
         { "usage",              no_argument,        NULL, 'u' },
         { "input",              required_argument,  NULL, 'i' },
@@ -118,12 +101,12 @@ status_t testSetupEncDataByArgs(EncTestData *data, int argc, char **argv) {
         { NULL,                 0,                  NULL, 0 }
     };
 
-    data->fp_input = NULL;
-    data->fp_output = NULL;
-    data->width = 0;
-    data->height = 0;
-    data->frameRate = 0;
-    data->bitRate = 0;
+    cmd->fp_input = NULL;
+    cmd->fp_output = NULL;
+    cmd->width = 0;
+    cmd->height = 0;
+    cmd->frameRate = 0;
+    cmd->bitRate = 0;
 
     while (true) {
         int optionIndex = 0;
@@ -136,32 +119,32 @@ status_t testSetupEncDataByArgs(EncTestData *data, int argc, char **argv) {
         case 'u':
             return BAD_VALUE;
         case 'i':
-            strcpy(data->file_input, optarg);
-            data->fp_input = fopen(data->file_input, "rb+");
-            if (data->fp_input == NULL) {
-                ALOGD("failed to open input file %s", data->file_input);
+            strcpy(cmd->file_input, optarg);
+            cmd->fp_input = fopen(cmd->file_input, "rb+");
+            if (cmd->fp_input == NULL) {
+                fprintf(stderr, "failed to open input file %s\n", cmd->file_input);
                 return BAD_VALUE;
             }
             break;
         case 'o':
-            strcpy(data->file_output, optarg);
-            data->fp_output = fopen(data->file_output, "wb+");
-            if (data->fp_input == NULL) {
-                ALOGD("failed to open output file %s", data->file_output);
+            strcpy(cmd->file_output, optarg);
+            cmd->fp_output = fopen(cmd->file_output, "wb+");
+            if (cmd->fp_output == NULL) {
+                fprintf(stderr, "failed to open output file %s\n", cmd->file_output);
                 return BAD_VALUE;
             }
             break;
         case 'w':
-            data->width = atoi(optarg);
+            cmd->width = atoi(optarg);
             break;
         case 'h':
-            data->height = atoi(optarg);
+            cmd->height = atoi(optarg);
             break;
         case 'b':
-            data->bitRate = atoi(optarg);
+            cmd->bitRate = atoi(optarg);
             break;
         case 'f':
-            data->frameRate = atoi(optarg);
+            cmd->frameRate = atoi(optarg);
             break;
         default:
             fprintf(stderr, "getopt_long returned unexpected value 0x%x\n", ic);
@@ -169,47 +152,44 @@ status_t testSetupEncDataByArgs(EncTestData *data, int argc, char **argv) {
         }
     }
 
-    if (data->fp_input == NULL || data->fp_output == NULL
-            || data->width == 0 || data->height == 0) {
-        ALOGE("ERROR: native_enc_test must specify input|output|width|height.");
+    if (cmd->fp_input == NULL || cmd->fp_output == NULL
+            || cmd->width == 0 || cmd->height == 0) {
+        fprintf(stderr, "ERROR: test must specify input|output|width|height");
         return BAD_VALUE;
     }
 
-    if (data->bitRate <= 0) {
-        data->bitRate = 3000000; // 3Mbps
+    if (cmd->bitRate <= 0) {
+        cmd->bitRate = 3000000; // 3Mbps
     }
-    if (data->frameRate <= 0) {
-        data->frameRate = 30; // 30fps
+    if (cmd->frameRate <= 0) {
+        cmd->frameRate = 30; // 30fps
     }
-    data->frameCount = 0;
-    data->colorFormat = OMX_COLOR_FormatYUV420SemiPlanar;
-    data->IDRInterval = 1;
-    data->timeout = 20 * 1000;
+    cmd->numBuffersEncoded = 0;
+    cmd->colorFormat = OMX_COLOR_FormatYUV420SemiPlanar;
+    cmd->IDRInterval = 1;
 
     // dump cmd options
     fprintf(stderr,
         "\ncmd parse result:\n"
-        "   input bitstream file:  %s\n"
+        "   input bitstream file : %s\n"
         "   output bitstream file: %s\n"
-        "   resolution:  %dx%d\n"
-        "   bitrare:     %d\n"
-        "   framerate:   %d\n",
-        data->file_input, data->file_output,
-        data->width, data->height, data->bitRate, data->frameRate);
+        "   resolution           : %dx%d\n"
+        "   bitrare              : %d\n"
+        "   framerate            : %d\n",
+        cmd->file_input, cmd->file_output,
+        cmd->width, cmd->height, cmd->bitRate, cmd->frameRate);
 
     return OK;
 }
 
-status_t testSetupCodec(sp<MediaCodec>* pCodec, EncTestData *data) {
-    status_t err;
-
+status_t testSetupCodec(EncTestArgs *data) {
     sp<ALooper> looper = new ALooper;
     looper->setName("native_enc_looper");
     looper->start();
 
     sp<MediaCodec> codec = MediaCodec::CreateByType(looper, kMimeTypeAvc, true);
     if (codec == NULL) {
-        ALOGD("ERROR: unable to create %s codec instance", kMimeTypeAvc);
+        fprintf(stderr, "ERROR: unable to create codec instance.\n");
         return UNKNOWN_ERROR;
     }
 
@@ -224,11 +204,11 @@ status_t testSetupCodec(sp<MediaCodec>* pCodec, EncTestData *data) {
 
     ALOGD("configure output format is '%s'", format->debugString(0).c_str());
 
-    err = codec->configure(format, NULL, NULL,
+    status_t err = codec->configure(format, NULL, NULL,
             MediaCodec::CONFIGURE_FLAG_ENCODE);
     if (err != NO_ERROR) {
-        ALOGD("ERROR: unable to configure %s codec at %dx%d (err=%d)",
-              kMimeTypeAvc, data->width, data->height, err);
+        fprintf(stderr, "ERROR: unable to configure codec at %dx%d (err=%d)\n",
+                data->width, data->height, err);
         codec->release();
         return err;
     }
@@ -241,12 +221,12 @@ status_t testSetupCodec(sp<MediaCodec>* pCodec, EncTestData *data) {
     ALOGD("got %zu input and %zu output buffers",
           data->inBuffers.size(), data->outBuffers.size());
 
-    *pCodec = codec;
+    data->codec = codec;
 
     return OK;
 }
 
-status_t testEncodeRun(sp<MediaCodec> encoder, EncTestData *data) {
+status_t testEncRun(EncTestArgs *data) {
     status_t err;
     int32_t pkt_size, read_size;
     char *pkt_buf = NULL;
@@ -271,14 +251,14 @@ status_t testEncodeRun(sp<MediaCodec> encoder, EncTestData *data) {
         }
 
         if (!sawInputEOS) {
-            err = encoder->dequeueInputBuffer(&index, data->timeout);
+            err = data->codec->dequeueInputBuffer(&index, kTimeout);
             if (err == OK) {
                 ALOGV("filling input buffer %zu", index);
                 const sp<ABuffer> &buffer = data->inBuffers.itemAt(index);
                 memcpy(buffer->base(), pkt_buf, read_size);
                 buffer->setRange(0, read_size);
 
-                err = encoder->queueInputBuffer(index, 0, buffer->size(), 0, 0);
+                err = data->codec->queueInputBuffer(index, 0, buffer->size(), 0, 0);
 
                 CHECK_EQ(err, (status_t)OK);
                 lastPktQueued = true;
@@ -287,10 +267,10 @@ status_t testEncodeRun(sp<MediaCodec> encoder, EncTestData *data) {
             }
         } else {
             if (!signalledInputEOS) {
-                err = encoder->dequeueInputBuffer(&index, data->timeout);
+                err = data->codec->dequeueInputBuffer(&index, kTimeout);
                 if (err == OK) {
                     ALOGD("signalling input EOS");
-                    err = encoder->queueInputBuffer(index, 0, 0,
+                    err = data->codec->queueInputBuffer(index, 0, 0,
                              0, MediaCodec::BUFFER_FLAG_EOS);
 
                     CHECK_EQ(err, (status_t)OK);
@@ -304,9 +284,9 @@ status_t testEncodeRun(sp<MediaCodec> encoder, EncTestData *data) {
         int64_t presentationTimeUs;
         uint32_t flags;
 
-        err = encoder->dequeueOutputBuffer(
+        err = data->codec->dequeueOutputBuffer(
                     &index, &offset, &size,
-                    &presentationTimeUs, &flags, data->timeout);
+                    &presentationTimeUs, &flags, kTimeout);
         if (err == OK) {
             ALOGV("draining output buffer %zu, time = %lld us",
                   index, (long long)presentationTimeUs);
@@ -319,17 +299,17 @@ status_t testEncodeRun(sp<MediaCodec> encoder, EncTestData *data) {
                 signalledOutputEOS = true;
                 ALOGD("reached EOS on output.");
             }
-            data->frameCount += 1;
-            err = encoder->releaseOutputBuffer(index);
+            data->numBuffersEncoded += 1;
+            err = data->codec->releaseOutputBuffer(index);
             CHECK_EQ(err, (status_t)OK);
         } else if (err == INFO_OUTPUT_BUFFERS_CHANGED) {
             ALOGD("INFO_OUTPUT_BUFFERS_CHANGED");
             CHECK_EQ((status_t)OK,
-                     encoder->getOutputBuffers(&data->outBuffers));
+                     data->codec->getOutputBuffers(&data->outBuffers));
             ALOGD("got %zu output buffers", data->outBuffers.size());
         } else if (err == INFO_FORMAT_CHANGED) {
             sp<AMessage> format;
-            CHECK_EQ((status_t)OK, encoder->getOutputFormat(&format));
+            CHECK_EQ((status_t)OK, data->codec->getOutputFormat(&format));
             ALOGD("INFO_FORMAT_CHANGED: %s", format->debugString().c_str());
         } else {
             CHECK_EQ(err, -EAGAIN);
@@ -339,49 +319,60 @@ status_t testEncodeRun(sp<MediaCodec> encoder, EncTestData *data) {
             break;
     }
 
+    free(pkt_buf);
+
     return 0;
 }
 
 int main(int argc, char **argv) {
     status_t err = OK;;
-    sp<MediaCodec> encoder;
-    EncTestData tData;
+    EncTestArgs cmd;
+    int64_t startTimeUs, elapsedTimeUs;
 
     // parse the cmd option
     if (argc > 1)
-        err = testSetupEncDataByArgs(&tData, argc, argv);
+        err = testParseArgs(&cmd, argc, argv);
 
     if (err != OK) {
         testUsage();
-        return 1;
+        goto ENCODE_OUT;
     }
 
-   // Start Binder thread pool.  MediaCodec needs to be able to receive
-   // messages from mediaserver.
-   sp<ProcessState> self = ProcessState::self();
-   self->startThreadPool();
+    // Start Binder thread pool.  MediaCodec needs to be able to receive
+    // messages from mediaserver.
+    ProcessState::self()->startThreadPool();
 
     ALOGD("creating codec");
-    err = testSetupCodec(&encoder, &tData);
-    if (err != OK)
-        return err;
-
-    time_start_record();
-
-    err = testEncodeRun(encoder, &tData);
+    err = testSetupCodec(&cmd);
     if (err != OK) {
-        ALOGD("ERROR: enc_test_run quit (err=%d)", err);
-    } else {
-        uint64_t consumes = time_end_record();
-        ALOGD("native_enc_test success total frame %d use_time %lld ms",
-              tData.frameCount, consumes);
+        fprintf(stderr, "ERROR: setup codec failed(err=%d)", err);
+        goto ENCODE_OUT;
     }
 
-//ENCODE_OUT:
+    startTimeUs = ALooper::GetNowUs();
+
+    ALOGD("start codec test");
+    err = testEncRun(&cmd);
+    if (err != OK) {
+        fprintf(stderr, "ERROR: enc_test_run failed(err=%d)", err);
+    } else {
+        elapsedTimeUs = ALooper::GetNowUs() - startTimeUs;
+        printf("\nenc_test done, %d frame encoded in %lld ms, %.2f fps\n",
+               cmd.numBuffersEncoded, elapsedTimeUs / 1000,
+               cmd.numBuffersEncoded * 1E6 / elapsedTimeUs);
+    }
+
+ENCODE_OUT:
     ALOGD("release codec");
 
-    encoder->stop();
-    encoder->release();
+    cmd.codec->stop();
+    cmd.codec->release();
+
+    if (cmd.fp_input != NULL)
+        fclose(cmd.fp_input);
+
+    if (cmd.fp_output != NULL)
+        fclose(cmd.fp_output);
 
     return 0;
 }
